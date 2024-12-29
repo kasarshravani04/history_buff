4c6463320fb5bfe0a5fa6064c80d265574fcce52
kasarshravani04 <aniket.yevankar@gmail.com>
2024-12-29 17:06:15 +0530

    adding support of numbersapi

--- first.py
+++ first.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import requests
 
 st.title("Welcome all to history buff!")
 st.header("Just add the date and I will find out what happened on that day in history.")
 
 choosed_date = st.date_input("Choose Date", value="today", min_value=None, max_value=None, key=None, help=None, on_change=None, args=None, kwargs=None, format="YYYY/MM/DD", disabled=False, label_visibility="visible")
 
-print(choosed_date)
+month = choosed_date.strftime("%m")
+day = choosed_date.strftime("%d")
+
+# API URL
+url = f"http://numbersapi.com/{month}/{day}/date"
+
+# Send request
+response = requests.get(url)
+
+# Check response
+if response.status_code == 200:
+    # print(f"On this day: {response.text}")
+    st.write(f"On this day: {response.text}")
+else:
+    # print(f"Error: {response.status_code}")
+    st.write(f"Error: {response.status_code}")
